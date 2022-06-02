171d18cd3c005efd6e622f016a934d352ef5f7b3
n8b5150 <87131703+n8b5150@users.noreply.github.com>
2022-06-02 11:38:59 -0400

    added client side code

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-alert('working')
+document.querySelector('h1').addEventListener('click', apiRequest)
+
+async function apiRequest(){
+    const parkName = document.querySelector('input').value
+    console.log(parkName)
+    try{
+        const response = await fetch(`https://testing-api-100devs.herokuapp.com/api/${parkName}`)
+        const data = await response.json()
+        console.log(data)
+        document.querySelector('a').href = data
+    }
+    catch(error){
+        console.log(error)
+    }
+}
